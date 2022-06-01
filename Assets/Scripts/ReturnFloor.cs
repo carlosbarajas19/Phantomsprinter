262caf41c8b07e2128645591db60317d160057ed
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-06-01 02:14:17 -0600

    small course

---
+++ Assets/Scripts/ReturnFloor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnFloor : MonoBehaviour
+{
+    [SerializeField] Transform ReturnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = ReturnPoint.position;
+    }
+}
